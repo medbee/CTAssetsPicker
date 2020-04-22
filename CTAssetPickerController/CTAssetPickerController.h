1ee7c63bdc1c41a07c8dba2f1b1b41b62130dd1d
Patrick-Kladek <patrick.kladek@outlook.com>
2020-04-22 09:53:24 +0200

    add CTAssetPickerController framework target

---
+++ CTAssetPickerController/CTAssetPickerController.h
@@ -0,0 +1,19 @@
+//
+//  CTAssetPickerController.h
+//  CTAssetPickerController
+//
+//  Created by Patrick Kladek on 22.04.20.
+//  Copyright © 2020 Clement T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CTAssetPickerController.
+FOUNDATION_EXPORT double CTAssetPickerControllerVersionNumber;
+
+//! Project version string for CTAssetPickerController.
+FOUNDATION_EXPORT const unsigned char CTAssetPickerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTAssetPickerController/PublicHeader.h>
+
+
