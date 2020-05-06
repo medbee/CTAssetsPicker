e29617526ced0be6f73057abc48c428b74654367
Patrick-Kladek <patrick.kladek@outlook.com>
2020-05-06 14:23:32 +0200

    make CTAssetsPickerController public

--- CTAssetsPicker/CTAssetsPicker.h
+++ CTAssetsPicker/CTAssetsPicker.h
@@ -32,30 +32,4 @@ FOUNDATION_EXPORT double CTAssetsPickerVersionNumber;
 //! Project version string for CTAssetsPickerController.
 FOUNDATION_EXPORT const unsigned char CTAssetsPickerVersionString[];
 
-#import <CTAssetsPicker/CTAssetCheckmark.h>
-#import <CTAssetsPicker/CTAssetCollectionViewCell.h>
-#import <CTAssetsPicker/CTAssetCollectionViewController.h>
-#import <CTAssetsPicker/CTAssetItemViewController.h>
-#import <CTAssetsPicker/CTAssetPlayButton.h>
-#import <CTAssetsPicker/CTAssetScrollView.h>
-#import <CTAssetsPicker/CTAssetSelectionButton.h>
-#import <CTAssetsPicker/CTAssetSelectionLabel.h>
-#import <CTAssetsPicker/CTAssetsGridSelectedView.h>
-#import <CTAssetsPicker/CTAssetsGridView.h>
-#import <CTAssetsPicker/CTAssetsGridViewCell.h>
-#import <CTAssetsPicker/CTAssetsGridViewController.h>
-#import <CTAssetsPicker/CTAssetsGridViewFooter.h>
-#import <CTAssetsPicker/CTAssetsGridViewLayout.h>
-#import <CTAssetsPicker/CTAssetsNavigationController.h>
-#import <CTAssetsPicker/CTAssetsPageView.h>
-#import <CTAssetsPicker/CTAssetsPageViewController.h>
-#import <CTAssetsPicker/CTAssetsPickerAccessDeniedView.h>
 #import <CTAssetsPicker/CTAssetsPickerController.h>
-#import <CTAssetsPicker/CTAssetsPickerDefines.h>
-#import <CTAssetsPicker/CTAssetsPickerNoAssetsView.h>
-#import <CTAssetsPicker/CTAssetsViewControllerTransition.h>
-#import <CTAssetsPicker/CTAssetThumbnailOverlay.h>
-#import <CTAssetsPicker/CTAssetThumbnailStacks.h>
-#import <CTAssetsPicker/CTAssetThumbnailView.h>
-
-
